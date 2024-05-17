7611d66171fdefc32f51c12a77a25dc504c917e3
fumiya ishihara <f-ishihara@wevnal.co.jp>
2024-05-17 19:26:24 +0900

    Add filter by tag

--- notion.js
+++ notion.js
@@ -42,3 +42,22 @@ chrome.commands.onCommand.addListener(function (command) {
     });
   }
 });
+
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "click_filter_by_tag") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: `
+          (function() {
+            const filterByTagSpan = document.querySelector('span[style="max-width: 180px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"]');
+            
+            if (filterByTagSpan) {
+              filterByTagSpan.click();
+              console.log('filterByTagSpan clicked', filterByTagSpan);
+            }
+          })();
+        `,
+      });
+    });
+  }
+});
